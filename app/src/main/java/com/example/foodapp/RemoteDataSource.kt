786557274e2a090951cd0501b19278b28951be5d
MhammadAli <Mohammed180726@feng.bu.edu.eg>
2023-05-22 19:17:56 +0300

    Inject RemoteDataSource

--- app/src/main/java/com/example/foodapp/RemoteDataSource.kt
+++ app/src/main/java/com/example/foodapp/RemoteDataSource.kt
@@ -1,4 +1,13 @@
 package com.example.foodapp
 
-class RemoteDataSource {
+import com.example.foodapp.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> =
+        foodRecipesApi.getRecipes(queries)
 }
