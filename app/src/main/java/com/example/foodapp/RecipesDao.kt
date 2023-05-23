d7b7d7169587582dc851594eb6ad86713212b485
MhammadAli <Mohammed180726@feng.bu.edu.eg>
2023-05-23 16:16:22 +0300

    Create RecipesDao interface

---
+++ app/src/main/java/com/example/foodapp/RecipesDao.kt
@@ -0,0 +1,18 @@
+package com.example.foodapp
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface RecipesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipes(recipesEntity: RecipesEntity)
+
+    @Query("select * from recipes_table order by id asc")
+    fun readRecipes(): Flow<List<RecipesEntity>>
+}
