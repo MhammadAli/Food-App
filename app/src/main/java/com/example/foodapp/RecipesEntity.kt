c016fa4f63da6f653ea7c59b11cd08010629414f
MhammadAli <Mohammed180726@feng.bu.edu.eg>
2023-05-23 16:09:48 +0300

    Create RecipesEntity class

---
+++ app/src/main/java/com/example/foodapp/RecipesEntity.kt
@@ -0,0 +1,14 @@
+package com.example.foodapp
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.foodapp.models.FoodRecipe
+import com.example.foodapp.util.Constants.RECIPES_TABLE
+
+
+@Entity(tableName = RECIPES_TABLE)
+data class RecipesEntity(
+    val foodRecipe: FoodRecipe,
+    @PrimaryKey(autoGenerate = false)
+    val id: Int = 0
+)
